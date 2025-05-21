b39910f0ec2c51316f6fc947f1a7fa463b130363
ObiajuluM <oambanefo@gmail.com>
2025-05-21 18:25:56 +0100

    preps

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +10,18 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Welcome to safety ally'),
+          child: Container(
+            color: Colors.blue,
+            child: Text(
+              'Welcome to Safety Ally',
+              style: GoogleFonts.josefinSans(
+                fontSize: 50,
+              ),
+            ),
+          ),
         ),
       ),
     );
